9ca461c91998d814ab05bcbec991cbdc77bc6bc7
Bittersweet66 <2816719412@qq.com>
2026-07-04 16:04:27 +0800

    remove the ignore of config

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+DB_CONFIG = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '123456',
+    'database': 'BookOrderDB',
+    'autocommit': False,
+    'charset': 'utf8mb4'
+}
